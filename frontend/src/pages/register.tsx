033d5fa60c0ab5f44f40e6a34ff8ccd7fbb656fa
Jonathan <Jonathan@iMac.local>
2022-06-03 15:21:14 -0400

    added input fields

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -3,35 +3,52 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Button,
+  Box,
 } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import React from "react";
+import InputField from "../components/InputField";
+import Wrapper from "../components/Wrapper";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
   return (
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ values, handleChange }) => (
-        <Form>
-          <FormControl>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            <Input
-              value={values.username}
-              onChange={handleChange}
-              id="username"
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
               placeholder="username"
+              label="Username"
             />
-            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-          </FormControl>
-        </Form>
-      )}
-    </Formik>
+            <Box>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              type="submit"
+              colorScheme="teal"
+              mt={4}
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
 };
 
